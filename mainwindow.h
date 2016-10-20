da7ec4b932da1c2f7d70a4a87225ae23a4dcebd2
naelstrof <naelstrof@gmail.com>
2016-10-20 16:05:19 -0500

    ripped out example code

--- mainwindow.h
+++ mainwindow.h
@@ -20,20 +20,13 @@ protected:
     void closeEvent(QCloseEvent *event) Q_DECL_OVERRIDE;
 
 private slots:
-    void open();
-    void save();
-    void penColor();
-    void penWidth();
     void imageSize();
     void finishImageSize(int w, int h);
-    void about();
 
 private:
     ImageSizeDialog* d;
     void createActions();
     void createMenus();
-    bool maybeSave();
-    bool saveFile(const QByteArray &fileFormat);
 
     DrawArea *scribbleArea;
 
@@ -43,20 +36,12 @@ private:
     QMenu *fileMenu;
     QMenu *editMenu;
     QMenu *optionMenu;
-    QMenu *helpMenu;
 
     QAction *undoAct;
     QAction *redoAct;
     QAction *imageSizeAct;
-    QAction *openAct;
-    QList<QAction *> saveAsActs;
     QAction *exitAct;
-    QAction *penColorAct;
-    QAction *penWidthAct;
-    QAction *printAct;
     QAction *clearScreenAct;
-    QAction *aboutAct;
-    QAction *aboutQtAct;
 };
 
 #endif
