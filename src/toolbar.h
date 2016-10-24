e67fddfacd85aa0781a6e9eab95041a890b3572f
Bernie Cosgriff <bernardcosgriff@gmail.com>
2016-10-23 23:00:12 -0600

    Qt didnt push .h

--- src/toolbar.h
+++ src/toolbar.h
@@ -2,6 +2,8 @@
 #define TOOLBAR_H
 
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
 
 class Toolbar : public QWidget {
     Q_OBJECT
@@ -9,8 +11,22 @@ public:
     explicit Toolbar(QWidget *parent = 0);
 
 signals:
+    QPushButton::toggled(bool checked);
 
 public slots:
+    void setCheckedButtons(bool checked);
+
+private:
+    QVBoxLayout* toolsLayout;
+
+    QPushButton* moveButton;
+    QPushButton* brushButton;
+    QPushButton* fillButton;
+    QPushButton* colorButton;
+    QPushButton* currentButton;
+
+    void initWidgets();
+    void initConnections();
 };
 
 #endif // TOOLBAR_H
