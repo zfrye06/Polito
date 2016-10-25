f37be6ce3cc439ecd8ad461897a9382eb0d1a8f0
Christian Felt <christianfelt@comcast.net>
2016-10-25 16:24:22 -0600

    uncommented toolbar

--- src/toolbar.h
+++ src/toolbar.h
@@ -1,32 +1,32 @@
-//#ifndef TOOLBAR_H
-//#define TOOLBAR_H
+#ifndef TOOLBAR_H
+#define TOOLBAR_H
 
-//#include <QWidget>
-//#include <QVBoxLayout>
-//#include <QPushButton>
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QPushButton>
 
-//class Toolbar : public QWidget {
-//    Q_OBJECT
-//public:
-//    explicit Toolbar(QWidget *parent = 0);
+class Toolbar : public QWidget {
+    Q_OBJECT
+public:
+    explicit Toolbar(QWidget *parent = 0);
 
-//signals:
-//    QPushButton::toggled(bool checked);
+signals:
+    QPushButton::toggled(bool checked);
 
-//public slots:
-//    void setCheckedButtons(bool checked);
+public slots:
+    void setCheckedButtons(bool checked);
 
-//private:
-//    QVBoxLayout* toolsLayout;
+private:
+    QVBoxLayout* toolsLayout;
 
-//    QPushButton* moveButton;
-//    QPushButton* brushButton;
-//    QPushButton* fillButton;
-//    QPushButton* colorButton;
-//    QPushButton* currentButton;
+    QPushButton* moveButton;
+    QPushButton* brushButton;
+    QPushButton* fillButton;
+    QPushButton* colorButton;
+    QPushButton* currentButton;
 
-//    void initWidgets();
-//    void initConnections();
-//};
+    void initWidgets();
+    void initConnections();
+};
 
-//#endif // TOOLBAR_H
+#endif // TOOLBAR_H
